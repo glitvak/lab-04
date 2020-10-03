b05c733add753c76cf6888a05e8929712393184f
Gil Litvak <43884803+glitvak@users.noreply.github.com>
2020-10-03 15:22:04 -0400

    init variables

--- app/src/main/java/com/example/lab04/MainActivity.java
+++ app/src/main/java/com/example/lab04/MainActivity.java
@@ -1,10 +1,25 @@
 package com.example.lab04;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
+    private final LinkedList<String> wordList = new LinkedList<>();
+    private final LinkedList<String> imgList = new LinkedList<>();
+    private final LinkedList<String> infoList = new LinkedList<>();
+
+    private RecyclerView mRecyclerView;
+    /*
+    ImageView image = findViewById(R.id.image);
+        Glide.with(this)
+                .asBitmap()
+                .load(imageUrl)
+                .into(image);
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
